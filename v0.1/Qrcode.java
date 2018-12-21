22c0e257bb9f676590ed64047ac5c2a514dc4f74
Walid Amriou <walidamriou@gmail.com>
2018-12-21 02:30:06 +0100

    Create Qrcode.java

---
+++ v0.1/Qrcode.java
@@ -0,0 +1,35 @@
+
+package qrcode;
+
+import javafx.scene.image.Image;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author walidamriou
+ */
+public class Qrcode extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Scene scene = new Scene(root);       
+        stage.getIcons().add(new Image("logo.png"));        
+        stage.setTitle("QRcode by walid amriou");
+        stage.setResizable(false);     
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
